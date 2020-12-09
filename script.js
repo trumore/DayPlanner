7dbfe78dcdfed31942cdac67c2a19ede709a13ff
Tom Rumore <rumoretom@gmail.com>
2020-12-09 17:55:08 -0500

    added button click function with console log to ensure functionality

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ $(document).ready(function () {
   var dt = luxon.DateTime.local();
 
   $("#currentDay").text(dt);
+
+  var toDo = $(".plans");
+  $(".saveBtn").on("click", function () {
+    console.log("save button clicked");
+  });
 });
 
 //if current hour = DateTime hour, current block= addClass.present
