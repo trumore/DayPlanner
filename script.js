393e95ed55fa058c3d5f22658597ad9811397ed3
Tom Rumore <rumoretom@gmail.com>
2020-12-10 13:24:27 -0500

    added save button functionality to save input and hour to local storage

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ $(document).ready(function () {
 
   $("#currentDay").text(dt);
 
-  var toDo = $(".plans");
   $(".saveBtn").on("click", function () {
+    let hour = $(this).siblings(".time-block").text();
+    let input = $(this).siblings(".plans").val();
+    console.log(input);
+    console.log(hour);
     console.log("save button clicked");
+    localStorage.setItem(hour, input);
   });
+
+  /*$(".time-block").each(function () {
+    var eachBlock = parseInt($(this).attr("id").split("-")[1]);
+    if (dt > eachBlock) {
+      $(this).addClass("#past");
+    }
+  });*/
+
+  //current hour will also be referenced using the dt variable
 });
 
 //if current hour = DateTime hour, current block= addClass.present
 //if current hour < DateTime hour, current block =  addClass.future
 //if current hour > DateTime hour, current block = addClass.past
+//use the
