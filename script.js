139ca7b9d78f50bb4ab959096082655bba84f532
Tom Rumore <rumoretom@gmail.com>
2020-12-10 15:21:45 -0500

    added function to display information held in local storage

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 $(document).ready(function () {
   var dt = luxon.DateTime.local();
+  var toDoList = [];
 
   $("#currentDay").text(dt);
 
   $(".saveBtn").on("click", function () {
-    let hour = $(this).siblings(".time-block").text();
-    let input = $(this).siblings(".plans").val();
-    console.log(input);
-    console.log(hour);
-    console.log("save button clicked");
+    var hour = $(this).siblings(".time-block").text();
+    var input = $(this).siblings(".plans").val();
+    //console.log(input);
+    //console.log(hour);
+    //console.log("save button clicked");
     localStorage.setItem(hour, input);
   });
 
+  function renderPlans() {
+    $(".time-block").each(function () {
+      var plans = $(this).text();
+      var storedPlans = localStorage.getItem(plans);
+      if (storedPlans !== null) {
+        $(this).siblings(".plans").val(storedPlans);
+      }
+    });
+  }
+  renderPlans();
+  var storedToDo = JSON.parse(localStorage.getItem("ToDoList"));
+
   /*$(".time-block").each(function () {
-    var eachBlock = parseInt($(this).attr("id").split("-")[1]);
+    var eachBlock = parseInt($(this).attr("id").split("hour")[1]);
     if (dt > eachBlock) {
-      $(this).addClass("#past");
+      $().addClass("#past");
     }
   });*/
 
